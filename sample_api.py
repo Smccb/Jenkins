8fb53319fc5b10c487809f8db22bcac9ff210c34
Smccb <sarahmmccb@gmail.com>
2024-03-17 23:57:10 +0000

    ..

--- sample_api.py
+++ sample_api.py
@@ -4,6 +4,7 @@ import json
 from pymongo import MongoClient
 from bson.json_util import dumps, loads
 from bson.objectid import ObjectId
+import graphene
 
 
 app = Flask(__name__)
@@ -19,11 +20,41 @@ class GetProducts(Resource):
         return json.loads(results)
 api.add_resource(GetProducts, '/getProducts')
 
+# Define GraphQL schema
+class Product(graphene.ObjectType):
+    id = graphene.ID()
+    title = graphene.String()
+    cost = graphene.Decimal()
+
+class Query(graphene.ObjectType):
+    product_titles = graphene.List(graphene.String)
+
+    def resolve_product_titles(root, info):
+        # Fetch product titles from MongoDB
+        client = MongoClient("mongodb://root:example@localhost:27017/")
+        db = client.Assignment1_webservices
+        collection = db.sales_data
+        products = collection.find({}, {"title": 1})
+        return [product['title'] for product in products]
+
 
 class GetTitles(Resource):
     def get(self):
-        return {'id': '111111'}
-                
+        # client = MongoClient("mongodb://root:example@localhost:27017/")
+        # db = client.sales
+        # collection = db.sales_data
+        # results = dumps(collection.find())
+        # return json.loads(results)
+
+        # Running a query on the data
+        schema = graphene.Schema(query=Query)
+        query = """
+        {
+            ProductTitle
+        }
+        """
+        result = schema.execute(query)
+        return(result.data)
 api.add_resource(GetTitles, '/getTitles')
 
 
